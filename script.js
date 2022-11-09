f82eda7e8c14532258e0f42686fd81b218f34445
david <azulaydavid2416@gmail.com>
2022-11-09 17:18:33 +0200

    library1

--- script.js
+++ script.js
@@ -1,21 +1,74 @@
 
-class user {
-  #firstName;
-  #lastName;
-  #hometown;
+
+class User {
+ 
     constructor(firstName,lastName,hometown){
-          this.#firstName = firstName;
-          this.#lastName = lastName;
-          this.#hometown = hometown;
+          this.firstName = firstName;
+          this.lastName = lastName;
+          this.hometown = hometown;
 
           this.borrowedBooks = [];
+         
     }
 
     addBook(newbook){
-      this.borrowedBooks.push(newbook)
-    }
+         
+           if(this.borrowedBooks.includes(newbook)){
+              newbook.canBorrow = false;
+           }
+           if(newbook.canBorrow==true){
+              this.borrowedBooks.push(newbook)
+           }
+          
+
+      
+}
+}
+class Book{
+
+  constructor(bookname,authorbook,canBorrow){
+    this.bookname = bookname;
+    this.authorbook = authorbook ;
+    this.canBorrow = canBorrow ;
+ 
+ } 
+
+
+}  
+
+class Report{
+  constructor(){
+   this.users = [];
+  }
+ 
+ 
+  adduser(newuser){
+    this.users.push(newuser)
+  }
+  
+  printreport(){
+    this.users.forEach((element) => {
+      console.log(element)
+    });
+  }
+
 }
 
-// let a = new user('david','azulay','tel aviv');
-// a.addBook('book')
-// console.log(a)
+
+
+
+let user1 = new User('david','azulay','tel aviv');
+let user2 = new User("eli", "moshe", "tel aviv");
+
+let book1 = new Book("lllll", "mmmm",false);
+let book2 = new Book('1984','gorge arwl',true);
+
+user1.addBook(book1)
+user1.addBook(book2)
+user2.addBook(book2)
+user1.addBook(book1)
+
+let report1 = new Report();
+report1.adduser(user1);
+report1.adduser(user2);
+report1.printreport();
